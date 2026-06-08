6270501cf22b5712dde0f18839290bcd2c5406a5
OSSAMARAMI <2003102600284@ofppt-edu.ma>
2026-06-08 11:43:23 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserCmcController;
+use App\Http\Controllers\Auth\MultiRegisterController;
+use App\Http\Controllers\Auth\PartenairesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,15 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::resource('UserCmc', UserCmcController::class);
+Route::resource('UserPartenaires', PartenairesController::class);
+
+Route::get('/register/cmc', [MultiRegisterController::class, 'showCmcForm'])->name('register.cmc');
+Route::post('/register/cmc', [MultiRegisterController::class, 'registerCmc']);
+
+Route::get('/register/partner', [MultiRegisterController::class, 'showPartnerForm'])->name('register.partner');
+Route::post('/register/partner', [MultiRegisterController::class, 'registerPartner']);
+
+
+
