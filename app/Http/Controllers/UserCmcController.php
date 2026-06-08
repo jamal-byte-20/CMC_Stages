2dcc9e617a6eb2a49112659d9a8263f141d9bf1d
OSSAMARAMI <2003102600284@ofppt-edu.ma>
2026-06-08 13:51:40 +0100

    ossama

--- app/Http/Controllers/UserCmcController.php
+++ app/Http/Controllers/UserCmcController.php
@@ -13,7 +13,6 @@ class UserCmcController extends Controller
     {
         //
     }
-
     /**
      * Show the form for creating a new resource.
      */
@@ -21,7 +20,6 @@ class UserCmcController extends Controller
     {
         //
     }
-
     /**
      * Store a newly created resource in storage.
      */
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserCmcController;
-use App\Http\Controllers\Auth\MultiRegisterController;
 use App\Http\Controllers\Auth\PartenairesController;
 
 Route::get('/', function () {
@@ -14,13 +13,12 @@ Route::get('/home', function () {
 });
 
 Route::resource('UserCmc', UserCmcController::class);
-Route::resource('UserPartenaires', PartenaireController::class);
 
-Route::get('/register/cmc', [MultiRegisterController::class, 'showCmcForm'])->name('register.cmc');
-Route::post('/register/cmc', [MultiRegisterController::class, 'registerCmc']);
+Route::get('/register/cmc', [UserCmcController::class, 'showCmcForm'])->name('register.cmc');
+Route::post('/register/cmc', [UserCmcController::class, 'registerCmc']);
 
-Route::get('/register/partner', [MultiRegisterController::class, 'showPartnerForm'])->name('register.partner');
-Route::post('/register/partner', [MultiRegisterController::class, 'registerPartner']);
+Route::get('/register/partner', [PartenairesController::class, 'showPartnerForm'])->name('register.partner');
+Route::post('/register/partner', [PartenairesController::class, 'registerPartner']);
 
 
 
